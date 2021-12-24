a8c015baf5e95c97baee4c99de1140dd302c0d8e
printac <bprintac1@gmail.com>
2021-12-24 17:24:09 +0200

    final version

--- sratimTV.py
+++ sratimTV.py
@@ -1,8 +1,13 @@
 from time import sleep
 import pip._vendor.requests as requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
+browser = webdriver.Chrome()
+sleep(5)
 session = requests.Session()
-
 MovieToWatch = input('{*} enter movie to watch: ')
 
 MOVIE_NAME = {f"term": {MovieToWatch}}
@@ -26,11 +31,18 @@ TOKEN = str(session.get(PRE_WATCH_API_TIMER).text)
 print("loading... 30 Sec remmaing..")
 sleep(30)
 token_time_auth = session.get(f"http://api.sratim.tv/movie/watch/id/{movie_id}/token/{TOKEN}").text
+SRATIM_COOKIE = session.cookies.get_dict()
+print(SRATIM_COOKIE)
+COOKIE_DICT = {'name':'Sratim','value':SRATIM_COOKIE['Sratim'],'domain':'.sratim.tv'}
+print(COOKIE_DICT)
 URL_AUTH = token_time_auth[token_time_auth.index('?'):-3]
 
 print(f"{WATCH_URL}{movie_id}.mp4{URL_AUTH}")
-print(session.get(f"{WATCH_URL}{movie_id}.mp4{URL_AUTH}").status_code)
+
+browser.get("http://sratim.tv")
+browser.add_cookie(COOKIE_DICT)
+browser.get(f"{WATCH_URL}{movie_id}.mp4{URL_AUTH}")
 
 
-#get request to the movie url gets an status code 200(OK) but when opening it in broswer gets error 401
-#TO DO: FIX --- 401 Authorization Required -- only when copying url to broswer 
+sleep(50)
+
